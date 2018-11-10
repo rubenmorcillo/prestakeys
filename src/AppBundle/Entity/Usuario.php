fb94e500bc57c2be8796727d81bc696828b70944
Luis Ramón López <lrlopez@gmail.com>
2018-11-10 19:31:29 +0100

    Añadidas anotaciones de tipo en los atributos
    No es obligatorio, pero se recomienda

--- src/AppBundle/Entity/Usuario.php
+++ src/AppBundle/Entity/Usuario.php
@@ -14,26 +14,31 @@ class Usuario
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\Column(type="integer")
+     * @var int
      */
     private $id;
 
     /**
      * @ORM\Column(type="string")
+     * @var string
      */
     private $nombre;
 
     /**
      * @ORM\Column(type="string")
+     * @var string
      */
     private $apellidos;
 
     /**
      * @ORM\Column(type="boolean")
+     * @var bool
      */
     private $ordenanza;
 
     /**
      * @ORM\Column(type="boolean")
+     * @var bool
      */
     private $secretario;
 }

1cf44daef085427f9d7309e8a9f4d2477be8e202
Luis Ramón López <lrlopez@gmail.com>
2018-11-10 19:25:08 +0100

    Creadas las clases principales del diagrama

---
+++ src/AppBundle/Entity/Dependencia.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AppBundle\Entity;
+
+class Dependencia
+{
+    private $id;
+
+    private $nombre;
+}
---
+++ src/AppBundle/Entity/Llave.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Entity;
+
+class Llave
+{
+    private $id;
+
+    private $descripcion;
+
+    private $codigo;
+
+    private $fechaPrestamo;
+}

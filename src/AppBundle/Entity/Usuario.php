13761c877808f9300d991df98450d7b63d98fb0d
Luis Ramón López <lrlopez@gmail.com>
2018-11-10 19:27:25 +0100

    Añadidas las anotaciones de los atributos de la entidad

--- src/AppBundle/Entity/Usuario.php
+++ src/AppBundle/Entity/Usuario.php
@@ -10,13 +10,30 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Usuario
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
     private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $nombre;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $apellidos;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
     private $ordenanza;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
     private $secretario;
 }

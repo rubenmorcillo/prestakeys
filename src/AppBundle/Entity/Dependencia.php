343608852947d5a797db820bdbaf6a71ea0269da
Luis Ramón López <lrlopez@gmail.com>
2018-11-12 22:39:53 +0100

    Convertir Dependencia en entidad con sus getters y setters

--- src/AppBundle/Entity/Dependencia.php
+++ src/AppBundle/Entity/Dependencia.php
@@ -2,9 +2,51 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="dependencia")
+ */
 class Dependencia
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     * @var int
+     */
     private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
     private $nombre;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param string $nombre
+     * @return Dependencia
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
 }

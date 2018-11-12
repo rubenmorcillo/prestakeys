386a9618b297f0d353ef50781a18cb233e696130
Luis Ramón López <lrlopez@gmail.com>
2018-11-12 11:02:59 +0100

    Convertir Llave en entidad e incorporar sus anotaciones

--- src/AppBundle/Entity/Llave.php
+++ src/AppBundle/Entity/Llave.php
@@ -2,13 +2,99 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="llave")
+ */
 class Llave
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     * @var int
+     */
     private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
     private $descripcion;
 
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
     private $codigo;
 
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     * @var \DateTime
+     */
     private $fechaPrestamo;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * @param string $descripcion
+     * @return Llave
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+
+    /**
+     * @param string $codigo
+     * @return Llave
+     */
+    public function setCodigo($codigo)
+    {
+        $this->codigo = $codigo;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getFechaPrestamo()
+    {
+        return $this->fechaPrestamo;
+    }
+
+    /**
+     * @param \DateTime|null $fechaPrestamo
+     * @return Llave
+     */
+    public function setFechaPrestamo(\DateTime $fechaPrestamo = null)
+    {
+        $this->fechaPrestamo = $fechaPrestamo;
+        return $this;
+    }
 }

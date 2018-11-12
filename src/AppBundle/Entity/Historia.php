a6eef75e8f252cbb48d398809aa071f0eaab364d
Luis Ramón López <lrlopez@gmail.com>
2018-11-12 23:20:19 +0100

    Getters y setters de Historia

--- src/AppBundle/Entity/Historia.php
+++ src/AppBundle/Entity/Historia.php
@@ -41,4 +41,84 @@ class Historia
      * @var \DateTime
      */
     private $fechaDevolucion;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Llave
+     */
+    public function getLlave()
+    {
+        return $this->llave;
+    }
+
+    /**
+     * @param Llave $llave
+     * @return Historia
+     */
+    public function setLlave(Llave $llave)
+    {
+        $this->llave = $llave;
+        return $this;
+    }
+
+    /**
+     * @return Usuario
+     */
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+
+    /**
+     * @param Usuario $usuario
+     * @return Historia
+     */
+    public function setUsuario(Usuario $usuario)
+    {
+        $this->usuario = $usuario;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getFechaPrestamo()
+    {
+        return $this->fechaPrestamo;
+    }
+
+    /**
+     * @param \DateTime $fechaPrestamo
+     * @return Historia
+     */
+    public function setFechaPrestamo(\DateTime $fechaPrestamo)
+    {
+        $this->fechaPrestamo = $fechaPrestamo;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getFechaDevolucion()
+    {
+        return $this->fechaDevolucion;
+    }
+
+    /**
+     * @param \DateTime $fechaDevolucion
+     * @return Historia
+     */
+    public function setFechaDevolucion(\DateTime $fechaDevolucion)
+    {
+        $this->fechaDevolucion = $fechaDevolucion;
+        return $this;
+    }
 }

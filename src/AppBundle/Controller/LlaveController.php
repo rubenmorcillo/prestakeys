d2156c565eb15fb9b8467000dea33fec904b8810
Luis Ramón López <lrlopez@gmail.com>
2019-01-29 08:33:59 +0100

    Ejemplo de respuesta JSON

--- src/AppBundle/Controller/LlaveController.php
+++ src/AppBundle/Controller/LlaveController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Repository\LlaveRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LlaveController extends Controller
@@ -18,5 +19,15 @@ class LlaveController extends Controller
         return $this->render('llaves/listar_prestadas.html.twig', [
             'llaves' => $llaves
         ]);
+    }
+
+    /**
+     * @Route("/llave/prestadas/json", name="llave_listar_prestadas")
+     */
+    public function listarPrestadasJsonAction(LlaveRepository $llaveRepository)
+    {
+        $llaves = $llaveRepository->findPrestadasArray();
+
+        return new JsonResponse($llaves);
     }
 }

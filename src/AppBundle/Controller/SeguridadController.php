1a88822aeed04838cef30ed2d3ad043c8c9a3bd4
Luis Ramón López <lrlopez@gmail.com>
2019-02-19 08:50:24 +0100

    Mostrar mensaje de error si no se puede entrar

--- src/AppBundle/Controller/SeguridadController.php
+++ src/AppBundle/Controller/SeguridadController.php
@@ -4,16 +4,25 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SeguridadController extends Controller
 {
     /**
      * @Route("/entrar", name="usuario_entrar")
      */
-    public function indexAction()
+    public function indexAction(AuthenticationUtils $authUtils)
     {
+        // obtener el error de autenticación, si hay alguno
+        $error = $authUtils->getLastAuthenticationError();
+
+        $ultimoUsuario = $authUtils->getLastUsername();
+
         // formulario de entrada
-        return $this->render('seguridad/entrar.html.twig');
+        return $this->render('seguridad/entrar.html.twig', [
+            'error' => $error,
+            'ultimo_usuario' => $ultimoUsuario
+        ]);
     }
 
     /**
